adfebf98113f7dbe163bbf24006e08dc874888ce
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-05-04 12:44:26 +0200

    Update Histogram.java

--- Histogram.java
+++ Histogram.java
@@ -8,14 +8,14 @@ public class histogram {
     
     public static void main(String[] args) 
     {
-        
+         File selectedFile = TextParser.openFileSelector();
 
         File inputFile = new File (inputFileName);
         Scanner in = new Scanner (inputFile);
         PrintWriter out = new PrintWriter(outputFileName);
 
         
-        File selectedFile = TextParser.openFileSelector();
+       
         
         }
         
