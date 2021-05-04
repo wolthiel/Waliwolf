5c5bc743814afc062c67f0988388c25586ea6cd1
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-05-04 12:37:30 +0200

    Update Histogram.java

--- Histogram.java
+++ Histogram.java
@@ -1,6 +1,7 @@
 public class histogram {
     public static void main(String[] args) 
     {
-        
+        private static final String OUTPUT_LOCATION = "LOCATION";
+        private static final int SCALING_FACTOR = 100;
     }
 }
