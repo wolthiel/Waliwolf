11feb88286ef6292381d3479e94b37b264d464fd
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-05-04 12:40:03 +0200

    Update Histogram.java

--- Histogram.java
+++ Histogram.java
@@ -3,5 +3,12 @@ public class histogram {
     {
         private static final String OUTPUT_LOCATION = "LOCATION";
         private static final int SCALING_FACTOR = 100;
+        
+        public static void main(String[]args) {
+        
+        File selectedFile = TextParser.openFileSelector();
+        
+        }
+        
     }
 }
