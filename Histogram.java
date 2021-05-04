9bf552b25ba1911915ec82ccb3b0c6d784ab0931
Wolfgang <wolfgang.thiele@student.htw-berlin.de>
2021-05-04 12:57:26 +0200

    upload Wolfgang 12:57

--- Histogram.java
+++ Histogram.java
@@ -8,16 +8,25 @@ public class histogram {
     
     public static void main(String[] args) 
     {
-         File selectedFile = TextParser.openFileSelector();
+        File selectedFile = TextParser.openFileSelector();
 
         File inputFile = new File (inputFileName);
         Scanner in = new Scanner (inputFile);
         PrintWriter out = new PrintWriter(outputFileName);
 
-        
-       
-        
+        try{
+            Map<Character, Integer> dict = TextParser.parseFile(selectedFile);
+            geberateHistogram(dict, true);
+            } catch (IOException e) {
+                e.printStack();
+                }
+    
         }
         
     }
+    private static void generateHistogram(Map<Character, Integer> dict, boolean addSeperator) throws IOException{
+
+
+    }
+    
 }
