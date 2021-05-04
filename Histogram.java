5ecf6ef04cc4c50009899e72fc7ba1036c008404
Wolfgang <wolfgang.thiele@student.htw-berlin.de>
2021-05-04 12:38:55 +0200

    message

--- Histogram.java
+++ Histogram.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+import java.io.PrintWriter;
+
 public class histogram {
     public static void main(String[] args) 
     {
         
+
+
+
+
+
+        File inputFile = new File (inputFileName);
+        Scanner in = new Scanner (inputFile);
+        PrintWriter out = new PrintWriter(outputFileName);
     }
 }
