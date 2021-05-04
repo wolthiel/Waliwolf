361b7c08354d6478973a411681c1cd70d77f2087
S0568243 <82872622+S0568243@users.noreply.github.com>
2021-05-04 12:43:36 +0200

    Update Histogram.java

--- Histogram.java
+++ Histogram.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 import java.io.PrintWriter;
 
 public class histogram {
+    
+     private static final String OUTPUT_LOCATION = "LOCATION";
+     private static final int SCALING_FACTOR = 100;
+    
     public static void main(String[] args) 
     {
         
 
-
-
-
-
         File inputFile = new File (inputFileName);
         Scanner in = new Scanner (inputFile);
         PrintWriter out = new PrintWriter(outputFileName);
-        private static final String OUTPUT_LOCATION = "LOCATION";
-        private static final int SCALING_FACTOR = 100;
-        
-        public static void main(String[]args) {
+
         
         File selectedFile = TextParser.openFileSelector();
         
